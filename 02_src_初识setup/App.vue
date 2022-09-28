434fd78c9bd03e15aca5069bffb2532233925c4e
Ejing6747 <113498134+Ejing6747@users.noreply.github.com>
2022-09-28 17:06:07 +0800

    Add files via upload

---
+++ 02_src_初识setup/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h4>欢迎</h4>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>性别：{{ sex }}</h2>
+  <button @click="sayHello">说话</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  setup() {
+    let name = "张三";
+    let age = 18;
+    let sex = "男";
+    function sayHello() {
+      alert(`我叫${name},我${age}岁`);
+    }
+    return {
+      name,
+      age,
+      sex,
+      sayHello,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ 03_src_ref函数/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h4>欢迎</h4>
+  <h2>姓名：{{ name }}</h2>
+  <h2>年龄：{{ age }}</h2>
+  <h2>性别：{{ sex }}</h2>
+  <h2>职别：{{ job.type }}</h2>
+  <h2>薪资：{{ job.salary }}</h2>
+  <button @click="changeInfo">修改人员信息</button>
+  <button @click="changeJob">修改工作信息</button>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let name = ref("张三");
+    let age = ref(18);
+    let sex = ref("男");
+    let job = ref({
+      type: "前端工程师",
+      salary: "30k",
+    });
+
+    function changeInfo() {
+      // name是RefImpl类型=reference+implement （引用+实现）
+      name.value = "李四";
+    }
+    function changeJob() {
+      // RefImpl类型中的对象是proxy类型（数据代理对象），是windows拥有的类型
+      job.value.type = "后端工程师";
+    }
+
+    return {
+      name,
+      age,
+      sex,
+      job,
+      changeInfo,
+      changeJob,
+    };
+  },
+};
+</script>
+
+<style></style>
